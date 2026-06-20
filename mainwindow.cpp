45d8faa7109ae23b3be6a6b8a252ee354b20827d
hellboy6-66 <akuharenko21@gmail.com>
2026-06-20 09:34:25 +0500

    Второй коммит2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMouseEvent>
+#include <QPoint>
+#include <cstdio> // IWYU pragma: keep
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
     this->setWindowFlags(this->windowFlags() & ~Qt::WindowMaximizeButtonHint);
-
     setWindowFlags(windowFlags() | Qt::FramelessWindowHint);
+
+    QWidget *emptyWidget = new QWidget(ui->centralwidget);
+    emptyWidget->setGeometry(0, 0, 800, 40);
+    emptyWidget->setStyleSheet("background-color: red;");
+    emptyWidget->installEventFilter(this);
+
+    ui->x_but->raise();
 
 
 }
@@ -17,11 +26,26 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
-}
 
+}
 
 void MainWindow::on_x_but_clicked()
 {
     this->close();
 }
 
+bool MainWindow::eventFilter(QObject *watched, QEvent *event)
+{
+    if (event->type() == QEvent::MouseButtonPress) {
+        QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
+
+        if (mouseEvent->button() == Qt::LeftButton) {
+            int x = mouseEvent->position().x();
+            int y = mouseEvent->position().y();
+            printf("Клик на виджете: X = %d, Y = %d\n", x, y);
+            fflush(stdout);
+            return true;
+        }
+    }
+    return QMainWindow::eventFilter(watched, event);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,8 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QMouseEvent>
-#include <QPoint>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,12 +17,17 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
+protected:
+    bool eventFilter(QObject *watched, QEvent *event) override;
 
 private slots:
     void on_x_but_clicked();
+
 
 private:
     Ui::MainWindow *ui;
 
 };
+
+
 #endif // MAINWINDOW_H
